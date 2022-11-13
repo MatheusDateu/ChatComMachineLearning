64477b497c4ba7ebd148bcbaf6adf30c1b4c56d9
MatheusDateu <mdelmondes5@gmail.com>
2022-11-12 23:34:42 -0400

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatML
+{
+    class Usuario
+    {
+        private static BigInteger id;
+        private string nomeUsuario { get; set; }
+        private string senhaUsuario { get; set; }
+
+        public Usuario(string nomeUsuario, string senhaUsuario)
+        {
+            id += 1;
+            BigInteger idDesteUsuario = id;
+            this.nomeUsuario = nomeUsuario;
+            this.senhaUsuario = senhaUsuario;
+            Usuarios(idDesteUsuario, nomeUsuario, senhaUsuario);
+
+        }
+
+        private static List<string> Usuarios(BigInteger idDesteUsuario, string nomeDoUsuario, string senhaDoUsuario)
+        {
+            List<string> usuarios= new List<string>();
+            string usuarioNovo = idDesteUsuario.ToString() + "-" + nomeDoUsuario + "," + senhaDoUsuario + ";";
+            usuarios.Add(nomeDoUsuario);
+
+            return usuarios;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Acess(object sender, RoutedEventArgs e)
+        {
+            string userName = txt_Username.Text;
+            string passWord = stxt_Password.Password.ToString();
+
+            if(userName != "Admin" || passWord != "1234")
+                MessageBox.Show("Senha incorreta", "Oops");
+            else
+                Application.Current.Shutdown();
+        }
+    }
+}
